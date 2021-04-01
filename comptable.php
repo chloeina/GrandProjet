fa889a87b234a665bb57d0bb5960ceeaec7532b1
chloeina <55871216+chloeina@users.noreply.github.com>
2021-04-01 14:56:11 +0200

    Add files via upload

---
+++ comptable.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+	<title>Comptable</title>
+</head>
+
+<body>
+<?php
+  echo"Vous avez choisi : ";
+  $choix=$_POST["choix"];
+  switch($choix)
+  {
+	  case "1" : $choix;
+				break;
+   	  case "2" : $choix;
+				break;
+	  case "3" : $choix;
+				break;
+  }
+  echo"$choix";
+?>
+</body>
+</html>
