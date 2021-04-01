cf80d33c92818f4954d36c42ff272dba26441e18
leavignal <81757514+leavignal@users.noreply.github.com>
2021-04-01 15:15:26 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,26 @@
 <html>
-<?php
-  
+<?php 
+
+include();
+
+if(isset($_POST['submit'])){
+    
+    $uname=$_POST['username'];
+    $password=$_POST['password'];
+    
+    $sql="select * from loginform where user='".$uname."'AND Pass='".$password."' limit 1";
+    
+    $result=$connexion->query($sql);
+    
+    if($connexion->rowCount($result)>0){
+        //rediriger vers la page v ou c
+		echo "c'est fait";
+    }
+    else{
+        echo " Votre identifiant ou mot de passe est faux! ";
+        exit();
+    }
+        
+}
 ?>
 </html>
