efccfa90dafe299021e6eb3eb53603b9c3c7af82
Hinda02 <81757732+Hinda02@users.noreply.github.com>
2021-04-29 15:16:52 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -1,34 +1,67 @@
-<?php 
-	require("connecter.php");
-	
-	//on recupere les informations pour la connexion
-	$uname=$_POST['username'];
-	$password=$_POST['password'];
-	$fonction=$_POST['fonction'];
+﻿<?php
+ob_start();
+// Appel du script de connexion au serveur et à la base de données
+	require("connecter.php"); 
 
-	$sql="SELECT login FROM visiteur WHERE nom='$uname'";
-	$result = $connexion->query($sql);
-	$ligne = $result->fetch();
-	$password = $ligne['password'];
+// On récupère les données saisies dans le formulaire
+	$username = $_POST["username"];
+	$mdpSaisi = $_POST["password"];
+	//$profession = $_POST["profession"];
+
+// On récupère dans la base de données le mot de passe qui correspond au nom saisi par le visiteur
+	$sql = "select mdp from visiteur where login= '$username'";
+	$result = $connexion ->query($sql);
+	$ligne = $result ->fetch();
+	$motPasseBdd = $ligne['mdp'];
 	
-	if($password!=$motPasseBdd){
-		echo "Votre identifiant ou mot de passe est faux! ";
+	//TESTS
+	// On récupère dans la base de données la profession qui correspond au nom saisi par le visiteur
+	$sql2 = "select profession from visiteur where login= '$username'";
+	$result2 = $connexion ->query($sql2);
+	$ligne2 = $result2 ->fetch();
+	$profession = $ligne2['profession'];
+	//END TESTS
+	//IT WORKS
+
+	// On vérifie que le mot de passe saisi est identique à celui enregistré dans la base de données
+
+	if  ($mdpSaisi!=$motPasseBdd)
+	// Le mot de passe est différent de celui de la base utilisateur
+	{
+		//TEST //include('login.html');
+		echo 'Votre saisie est erronée, veuillez recommencer...'; 
+
+		// On inclut le formulaire d'identification (index.html)
 		include('login.html');
-		}
-		else{
-			session_start();
-			$_SESSION['connexion']="oui";
-			$_SESSION['username']=$uname;
-			
-			if($fonction=='comptable'){
-				header("location:Comptable.php");
 
-			 }else{
-				header("location:SaisieForfait.php");
-				}   
-			}	
-		//on libère le jeu d'enregistrement
-		$res->closeCursor();
-		// on ferme la connexion au SGBD
-		$connexion=null;
+		// On quitte le script courant sans effectuer les éventuelles instructions qui suivent
+		exit; 
+	}
+	else
+	// Le mot de passe saisi correspond à celui de la base utilisateur
+	{
+		if($profession == 'visiteur'){
+			
+			header("location:saisieForfait.html");
+		ob_end_flush();
+		// On quitte le script courant sans effectuer les éventuelles  instructions qui suivent
+		exit;
+		
+		}else{
+			
+			header("location:Comptable.html");
+		ob_end_flush();
+		// On quitte le script courant sans effectuer les éventuelles  instructions qui suivent
+		exit;
+		
+		}
+		
+	}
+	
+	//on libère le jeu d'enregistrement
+	$result ->closeCursor(); 
+	// on ferme la connexion au SGBD
+	$connexion = null;
+
+
 ?>
